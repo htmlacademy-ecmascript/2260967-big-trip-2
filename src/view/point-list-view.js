ac178c6db5c6dd7205e55edec461d475c9a141db
ypotapova03-pixel <ypotapova03@gmail.com>
2026-07-01 21:40:19 +0300

    1.25. Время разбивать камни

---
+++ src/view/point-list-view.js
@@ -0,0 +1,25 @@
+import {createElement} from '../render.js';
+
+function createPointList() {
+  return `
+   <ul class="trip-events__list"></ul>
+  `;
+}
+
+export default class PointListView {
+  getTemplate() {
+    return createPointList();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement(){
+    this.element = null;
+  }
+}
