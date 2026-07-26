3fb4f3a1ee1250f0abcaeb7b8779b41bed764d7f
ypotapova03-pixel <ypotapova03@gmail.com>
2026-07-26 15:56:36 +0300

    5.12. Большие перемены (часть 2)

--- src/view/point-sort-view.js
+++ src/view/point-sort-view.js
@@ -1,9 +1,10 @@
-import AbstractView from '../framework/view/abstract-view';
+import AbstractView from '../framework/view/abstract-view.js';
+
 function createPointSortForm() {
   return `
      <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
             <div class="trip-sort__item  trip-sort__item--day">
-              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
+              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" data-sort-type="day" checked>
               <label class="trip-sort__btn" for="sort-day">Day</label>
             </div>
 
@@ -13,12 +14,12 @@ function createPointSortForm() {
             </div>
 
             <div class="trip-sort__item  trip-sort__item--time">
-              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort-type="time">
               <label class="trip-sort__btn" for="sort-time">Time</label>
             </div>
 
             <div class="trip-sort__item  trip-sort__item--price">
-              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type="price">
               <label class="trip-sort__btn" for="sort-price">Price</label>
             </div>
 
@@ -31,7 +32,25 @@ function createPointSortForm() {
 }
 
 export default class PointSortView extends AbstractView {
+  #handleSortTypeChange = null;
+
+  constructor(onSortTypeChange) {
+    super();
+    this.#handleSortTypeChange = onSortTypeChange;
+
+    this.element.addEventListener('change', this.#sortTypeChangeHandler);
+  }
+
   get template() {
     return createPointSortForm();
   }
+
+  #sortTypeChangeHandler = (evt) => {
+    if (!evt.target.dataset.sortType) {
+      return;
+    }
+
+    evt.preventDefault();
+    this.#handleSortTypeChange(evt.target.dataset.sortType);
+  };
 }
